bdf9737d89cd3f5a6380e06c34a0f6ee49f29cfc
prisar <pritimaysarkar2010@gmail.com>
2020-01-23 00:26:17 +0530

    articles

---
+++ goservicedemo/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Descrition", Content: "Hello World"},
+	}
+	fmt.Println("All articles endpoint hit")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	handleRequests()
+}
