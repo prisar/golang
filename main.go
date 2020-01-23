d02f3d627513557ffd2e916f6bf2a7ed2615dd4c
prisar <pritimaysarkar2010@gmail.com>
2020-01-24 03:03:09 +0530

    auth middleware

--- main.go
+++ main.go
@@ -37,6 +37,11 @@ type User struct {
 	Email string
 }
 
+// Define our struct
+type authenticationMiddleware struct {
+	tokenUsers map[string]string
+}
+
 func InitialMigration() {
 	db, err := gorm.Open("sqlite3", "test.db")
 	if err != nil {
@@ -65,7 +70,6 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func AllUsers(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("all users called")
 	db, err := gorm.Open("sqlite3", "test.db")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -106,6 +110,40 @@ func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, `{"alive": true}`)
 }
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Println(r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Initialize it somewhere
+func (amw *authenticationMiddleware) Populate() {
+	amw.tokenUsers["00000000"] = "user0"
+	amw.tokenUsers["aaaaaaaa"] = "userA"
+	amw.tokenUsers["05f717e5"] = "randomUser"
+	amw.tokenUsers["deadbeef"] = "user0"
+}
+
+// Middleware function, which will be called for each request
+func (amw *authenticationMiddleware) Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("X-Session-Token")
+
+		if user, found := amw.tokenUsers[token]; found {
+			// We found the token in our map
+			log.Printf("Authenticated user %s\n", user)
+			// Pass down the request to the next middleware (or final handler)
+			next.ServeHTTP(w, r)
+		} else {
+			// Write an error and stop the handler chain
+			http.Error(w, "Forbidden", http.StatusForbidden)
+		}
+	})
+}
+
 func handleRequests() {
 	port := os.Getenv("PORT")
 
@@ -114,6 +152,13 @@ func handleRequests() {
 	}
 
 	router := mux.NewRouter().StrictSlash(true)
+
+	router.Use(loggingMiddleware)
+
+	amw := authenticationMiddleware{tokenUsers: make(map[string]string)}
+	amw.Populate()
+
+	router.Use(amw.Middleware)
 
 	router.HandleFunc("/api/health", HealthCheckHandler)
 
